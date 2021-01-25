ba0fd471da296440dfc09690937c0fdde0d184ff
YuQingXi <840174265@qq.com>
2021-01-25 16:34:44 +0800

    添加了Student类

---
+++ GitText/GitText/src/com/yqx/git/Student.java
@@ -0,0 +1,13 @@
+package com.yqx.git;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+
+    public static void main(String[] args) {
+
+        System.out.println();
+
+    }
+
+}
