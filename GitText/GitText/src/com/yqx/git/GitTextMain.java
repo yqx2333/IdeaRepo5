e04d3336ab1e0476e608c96517d30e822ac25be0
YuQingXi <840174265@qq.com>
2021-01-25 16:13:16 +0800

    新建项目 提交

---
+++ GitText/GitText/src/com/yqx/git/GitTextMain.java
@@ -0,0 +1,24 @@
+package com.yqx.git;
+
+public class GitTextMain {
+
+    public static void main ( String[] args ){
+
+        System.out.println( "Git分支测试" );
+        System.out.println( "Git分支测试" );
+        System.out.println( "Git分支测试" );
+        System.out.println( "Git分支测试" );
+        System.out.println( "Git分支测试" );
+        System.out.println( "Git分支测试" );
+        System.out.println( "Git分支测试" );
+        System.out.println( "Git分支测试" );
+        System.out.println( "Git分支测试" );
+        System.out.println( "Git分支测试" );
+        System.out.println( "Git分支测试" );
+        System.out.println( "Git分支测试" );
+        System.out.println( "Git分支测试" );
+
+    }
+
+
+}
